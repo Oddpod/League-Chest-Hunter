d73fdc01855d3b0111c75a940ef24ef57fc60296
Odd Harald Sjursen Sande <odd.sande@soprasteria.com>
2020-05-05 15:58:16 +0200

    Remove scoped model

--- lib/views/Home/CompletedChests.dart
+++ lib/views/Home/CompletedChests.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:league_chest_hunter/models/summoner.dart';
-import 'package:league_chest_hunter/state/summoner.dart';
 import 'package:league_chest_hunter/widgets/ChampGrid.dart';
 import 'package:provider/provider.dart';
-import 'package:scoped_model/scoped_model.dart';
 
 class CompletedChests extends StatelessWidget {
   CompletedChests({Key key, this.title}) : super(key: key);
